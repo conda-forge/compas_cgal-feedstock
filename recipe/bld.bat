24e1519d916c9e5c89da4748ae6a1dc9aa64455f
Petras Vestartas <petrasvestartas@gmail.com>
2025-12-18 20:36:26 +0100

    MemoryError

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,9 +1,10 @@
 @echo off
-set CMAKE_BUILD_PARALLEL_LEVEL=%CPU_COUNT%
+:: Limit parallel builds to prevent memory exhaustion on Windows CI
+:: CGAL template-heavy C++ compilation uses ~2GB per thread
+set CMAKE_BUILD_PARALLEL_LEVEL=2
 
 %PYTHON% -m pip install . -v
 if errorlevel 1 exit 1
 
 :: Clean up build directory to reduce size before conda-build stats collection
-:: This prevents MemoryError in directory_size() on Windows
 if exist "%SRC_DIR%\build" rd /s /q "%SRC_DIR%\build"
