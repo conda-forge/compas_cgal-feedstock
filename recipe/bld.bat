b7ce5af06f6dc2db5918de4f5caf47274aa46e2e
Petras Vestartas <petrasvestartas@gmail.com>
2025-12-18 20:57:22 +0100

    Reduce parallel builds to 1 to fix C1060 heap error

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Limit parallel builds to prevent memory exhaustion on Windows CI
-:: CGAL template-heavy C++ compilation uses ~2GB per thread
-set CMAKE_BUILD_PARALLEL_LEVEL=2
+:: CGAL template-heavy C++ compilation uses ~4GB per thread
+set CMAKE_BUILD_PARALLEL_LEVEL=1
 
 %PYTHON% -m pip install . -v
 if errorlevel 1 exit 1
